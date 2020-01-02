ab1faa28ee3542e9e0da181f677e23d767186ae3
Alexander Schwartzberg <aeksco@gmail.com>
2020-01-01 19:51:50 -0500

    Added example to Hello component to inject JS script into current chrome tab

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     semi: true,
-    trailingComma: 'all',
+    trailingComma: "all",
     singleQuote: false,
-    printWidth: 120,
     tabWidth: 4,
 };
--- src/components/hello/component.tsx
+++ src/components/hello/component.tsx
@@ -1,14 +1,67 @@
 import * as React from "react";
 import "./styles.scss";
 
+// // // //
+
+// Scripts to execute in current tab
+const toggleAllScript = `document.querySelectorAll(".js-details-target").forEach(c => { c.click(); });`;
+const closeAllScript = `document.querySelectorAll(".js-details-target[aria-expanded='true']").forEach(c => { c.click(); });`;
+const openAllScript = `document.querySelectorAll(".js-details-target[aria-expanded='false']").forEach(c => { c.click(); });`;
+
+/**
+ * Executes a string of Javascript on the current tab
+ * @param code The string of code to execute on the current tab
+ */
+function executeScript(code: string) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
+        const currentTab: any | undefined = tabs[0];
+        if (!currentTab) {
+            return;
+        }
+
+        chrome.tabs.executeScript(
+            currentTab.id,
+            {
+                code,
+            },
+            () => {
+                console.log("DONE");
+            },
+        );
+    });
+}
+
+// // // //
+
 export function Hello() {
     const [count, increment] = React.useReducer(i => i + 1, 0);
 
     return (
         <div className="row">
             <div className="col-lg-12">
-                <button className="btn btn-block btn-outline-success" onClick={increment}>
+                <button
+                    className="btn btn-block btn-outline-success"
+                    onClick={increment}
+                >
                     Clicked {count} times
+                </button>
+                <button
+                    className="btn btn-block btn-outline-success"
+                    onClick={() => executeScript(openAllScript)}
+                >
+                    Open All
+                </button>
+                <button
+                    className="btn btn-block btn-outline-success"
+                    onClick={() => executeScript(closeAllScript)}
+                >
+                    Close All
+                </button>
+                <button
+                    className="btn btn-block btn-outline-success"
+                    onClick={() => executeScript(toggleAllScript)}
+                >
+                    Toggle All
                 </button>
             </div>
         </div>
