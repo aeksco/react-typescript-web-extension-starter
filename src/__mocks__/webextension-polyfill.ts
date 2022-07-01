6860e95f420e41d206aeaa42f279afd1eba13157
Punith B M <punith@designstring.com>
2022-07-01 13:51:18 +0530

    storybook updated

--- src/__mocks__/webextension-polyfill.ts
+++ src/__mocks__/webextension-polyfill.ts
@@ -1,8 +1,8 @@
-// src/__mocks__/webextension-polyfill-ts
-// Update this file to include any mocks for the `webextension-polyfill-ts` package
+// src/__mocks__/webextension-polyfill
+// Update this file to include any mocks for the `webextension-polyfill` package
 // This is used to mock these values for Storybook so you can develop your components
 // outside the Web Extension environment provided by a compatible browser
-// See .storybook/main.js to see how this module is swapped in for `webextension-polyfill-ts`
+// See .storybook/main.js to see how this module is swapped in for `webextension-polyfill`
 export const browser: any = {
     tabs: {
         executeScript(currentTabId: number, details: any) {
